68567df8e73c92f4531beb89e81837669754c590
seraphin-FTC <efrictus@gmail.com>
2026-01-24 19:47:13 +0100

    Max Prog : Debut de rotation shooter, à compléter et terminer.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MaxRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MaxRobot.java
@@ -7,6 +7,17 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
 @TeleOp
 public class MaxRobot extends LinearOpMode {
 
@@ -20,13 +31,20 @@ public class MaxRobot extends LinearOpMode {
 
     CRServo servoMoteurRamassageBalle;
 
+    CRServo servoMoteurShooter;
+
     IMU imu;
+
+    private Position cameraPosition = new Position(DistanceUnit.INCH, 0, 0, 0, 0);
+    private YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES, 0, -90, 0, 0);
+    private VisionPortal visionPortal;
 
     private double vitesseDeplacement = 0.7;
 
     private boolean estRamassageActif = false;
 
-    private boolean etatPrecedentL1 = false;
+    private AprilTagProcessor aprilTag;
+
 
     @Override
     public void runOpMode() {
@@ -37,11 +55,60 @@ public class MaxRobot extends LinearOpMode {
             gestionVitesseDeplacement();
             deplacement();
             gestionRamassage();
-
+            rotationShooter();
             telemetry.update();
         }
 
         arretMoteurs();
+    }
+
+    private void rotationShooter() {
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one .
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                // Only use tags that don't have Obelisk in them
+                if (detection.metadata.name.contains("BlueTarget")) {
+                    telemetry.addLine(String.format("Centre %6.1f", detection.center.x));
+                    // Position de l'AprilTag dans l'image
+                    if (detection.center.x > 340) {
+                        telemetry.addLine(String.format("Tourner Droite"));
+                        double rotation = 0.1;
+                        tournerShooter(rotation);
+                    } else if (detection.center.x < 300) {
+                        telemetry.addLine(String.format("Tourner Gauche"));
+                        double rotation = -0.1;
+                        tournerShooter(rotation);
+                    } else {
+                        telemetry.addLine(String.format("Attendre"));
+                        double rotation = 0;
+                        tournerShooter(rotation);
+                    }
+                }
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+
+        }   // end for() loop
+        if (currentDetections.size()==0){
+            telemetry.addLine(String.format("\n==== Rien trouvé"));
+            double rotation = 0.1;
+            tournerShooter(rotation);
+        }
+
+        // Add "key" information to telemetry
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+
+    }
+
+    private void tournerShooter(double rotation) {
+        servoMoteurShooter.setPower(rotation);
+        telemetry.addLine(String.format("%.2f, Rotation Vitesse", rotation));
     }
 
     private void arretMoteurs() {
@@ -50,6 +117,7 @@ public class MaxRobot extends LinearOpMode {
         moteurArriereGauche.setPower(0);
         moteurArriereDroit.setPower(0);
         servoMoteurRamassageBalle.setPower(0);
+        servoMoteurShooter.setPower(0);
     }
 
     private void gestionRamassage() {
@@ -60,7 +128,7 @@ public class MaxRobot extends LinearOpMode {
         }
 
         if (estRamassageActif) {
-            servoMoteurRamassageBalle.setPower(0.1);
+            servoMoteurRamassageBalle.setPower(-1.0);
         } else {
             servoMoteurRamassageBalle.setPower(0.0);
         }
@@ -88,6 +156,7 @@ public class MaxRobot extends LinearOpMode {
         moteurArriereGauche = hardwareMap.get(DcMotor.class, "dosGauche");
 
         servoMoteurRamassageBalle = hardwareMap.get(CRServo.class, "ramassage");
+        servoMoteurShooter = hardwareMap.get(CRServo.class, "rotationShooter");
 
         moteurAvantDroit.setDirection(DcMotor.Direction.REVERSE);
         moteurAvantGauche.setDirection(DcMotor.Direction.FORWARD);
@@ -107,6 +176,16 @@ public class MaxRobot extends LinearOpMode {
                         RevHubOrientationOnRobot.UsbFacingDirection.UP
                 )
         ));
+
+        aprilTag = new AprilTagProcessor.Builder()
+                .setCameraPose(cameraPosition, cameraOrientation)
+                .build();
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        builder.addProcessor(aprilTag);
+        visionPortal = builder.build();
     }
 
     public void deplacement() {
